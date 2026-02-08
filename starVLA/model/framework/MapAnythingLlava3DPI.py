3a4731ddac32bead958602c4ab2060bb79f6756f
bozagina <bbbforbazinga@gmail.com>
2026-02-08 22:01:11 +0800

    tmp

--- starVLA/model/framework/MapAnythingLlava3DPI.py
+++ starVLA/model/framework/MapAnythingLlava3DPI.py
@@ -242,7 +242,7 @@ if __name__ == "__main__":
 
     cfg = OmegaConf.load(args.config_yaml)
     # try get model
-    cfg.framework.mapanything_llava3d.base_vlm = "./checkpoints/mapanything_llava3d_vlm_base"
+    cfg.framework.mapanything_llava3d.base_vlm = "/2025233147/zzq/SpatialVLA_llava3d/model_zoo/mapanythingllava3d_base"
     
 
     model = MapAnythingLlava3D_PI(cfg)
